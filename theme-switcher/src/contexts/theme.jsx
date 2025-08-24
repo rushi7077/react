0f88363e6a61255686c4ec0439b82f2e520304e0
Rushi Baravkar <rushibaravkar1012@gmail.com>
2025-08-24 11:16:20 +0530

    toggling

--- theme-switcher/src/contexts/theme.jsx
+++ theme-switcher/src/contexts/theme.jsx
@@ -1,43 +1,13 @@
-import { createContext, useContext, useState, useEffect } from "react";
+import { createContext, useContext } from "react";
 
 export const ThemeContext = createContext({
-  themeMode: "light",
-  darkTheme: () => {},
-  lightTheme: () => {}
-});
+    themeMode: "light",
+    darkTheme: () => {},
+    lightTheme: () => {},
+})
 
-export const ThemeProvider = ({ children }) => {
-  const [themeMode, setThemeMode] = useState(
-    localStorage.getItem("theme") || "light"
-  );
+export const ThemeProvider = ThemeContext.Provider
 
-  const darkTheme = () => {
-    setThemeMode("dark");
-    localStorage.setItem("theme", "dark");
-  };
-
-  const lightTheme = () => {
-    setThemeMode("light");
-    localStorage.setItem("theme", "light");
-  };
-
-  useEffect(() => {
-    if (themeMode === "dark") {
-      document.documentElement.classList.add("dark");
-      document.documentElement.classList.remove("light");
-    } else {
-      document.documentElement.classList.add("light");
-      document.documentElement.classList.remove("dark");
-    }
-  }, [themeMode]);
-
-  return (
-    <ThemeContext.Provider value={{ themeMode, darkTheme, lightTheme }}>
-      {children}
-    </ThemeContext.Provider>
-  );
-};
-
-export default function useTheme() {
-  return useContext(ThemeContext);
+export default function useTheme(){
+    return useContext(ThemeContext)
 }
